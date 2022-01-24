71f26a41bd599532d897bb3b508d5adcc297b58b
Matt Campbell <matt.campbell@classyllama.com>
2022-01-23 20:35:34 -0600

    Extend React with Prettier

--- react.js
+++ react.js
@@ -10,6 +10,7 @@ module.exports = {
 		'plugin:react/recommended',
 		'plugin:jsx-a11y/recommended',
 		'plugin:testing-library/react',
+		'plugin:prettier/recommended',
 	],
 	parserOptions: {
 		ecmaFeatures: {
